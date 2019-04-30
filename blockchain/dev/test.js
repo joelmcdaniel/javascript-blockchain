f4cb4868137611c21b4281bca828b3052dcb78f7
joelmcdaniel <joel.mcdaniel@live.com>
2019-04-30 00:03:47 -0700

    First commit

---
+++ blockchain/dev/test.js
@@ -0,0 +1,42 @@
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+console.log(bitcoin);
+
+/* const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'B4CEE9C0E5CD571',
+        recipient: '3A3F6E462D48E9',  
+    }  
+]
+const nonce = 100;
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)); */
+
+/* const previousBlockHash = 'OINAISDFN09N09ASDNF90N90ASNDF';
+
+const currentBlockData = [
+    {
+        amount: 101,
+        sender: 'N90ANS90N90ANSDFN',
+        recipient: '90NA90SNDF90ANSDF09N',  
+    },
+    {
+        amount: 30,
+        sender: '09ANS09DFNA8SDNF',
+        recipient: 'UIANSIUDFUIABSDUIFB',  
+    },
+    {
+        amount: 200,
+        sender: '89ANS89DFN98ASNDF89',
+        recipient: 'AUSDF89ANSD9FNASD',  
+    } 
+]; */
+//const nonce = 100;
+
+//console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData)); */
+// const nonce = 27470
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+
